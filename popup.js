8872842bc994cde7d9b8777c460a02501fa79ebf
0ManasVerma0 <manasverma1357@gmail.com>
2026-02-22 15:21:15 +0530

    add loadSaveSetting & checkSessionStatus fn. using chrome storage api

--- popup.js
+++ popup.js
@@ -18,6 +18,45 @@ const progress = document.getElementById("progressText")
 const siteName = document.getElementById("siteName")
 const progressFill = document.getElementById("progressFill")
 
+function loadSavedSettings(){
+    //using chrome storage API for storing the settings locally 
+    chrome.storage.local.get(["HabitSettings"], (result)=>{
+        console.log("Loading saved settings:", result)
+
+        if(result.HabitSettings){
+            const settings = result.HabitSettings;
+
+            if(settings.websiteUrl) websiteUrl.value = settings.websiteUrl;
+            if(settings.startTime) startTime.value = settings.startTime;
+            if(settings.duration) duration.value = settings.duration;
+            if(settings.totalDays) totalDays.value = settings.totalDays;
+
+            console.log("all your settings are loaded")
+        }
+        else{
+            console.log("No saved settings found(first time use)")
+        }
+    })
+}
+
+function checkSessionStatus(){
+    //using chrome storage api for checking the status 
+    chrome.runtime.sendMessage({action: 'getStatus'}, (response)=> {
+        console.log("session staus: ", response)
+
+        //IF ACTIVE SESSION
+        if(response && response.active){
+            showActiveView(response) //show active view
+        }
+        else{
+            //show setup view
+            showSetupView();
+        }
+    })
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log("popup opened")
     loadSavedSettings();
